7a8714977b6416ecc2e98f0fda3fbe231e02545c
rdaavee <rdavee0412@gmail.com>
2024-04-25 03:37:57 +0800

    init commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req, res) => {
+  res.json({
+    data: "This is signup endpoint",
+  });
+};
+
+export const login = async (req, res) => {
+  res.json({
+    data: "This is login endpoint",
+  });
+};
+
+export const logout = async (req, res) => {
+  res.json({
+    data: "This is logout endpoint",
+  });
+};
