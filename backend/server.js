4842651ca24725b9f86e3e53296734b5c0a1d064
rdaavee <rdavee0412@gmail.com>
2024-04-25 14:23:22 +0800

    added functionality for signup, login, and logout (controllers, models, jwt)

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
-import connectMongoDB from "../backend/db/connectMongoDB.js";
+import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use("/api/auth", authRoutes);
+app.use(express.json()); //to parse req.body in controller
+app.use(express.urlencoded({ extended: true })); //to parse from data
+app.use(cookieParser()); // to be able to get the cookies
+
+app.use("/api/auth", authRoutes); //middleware
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
