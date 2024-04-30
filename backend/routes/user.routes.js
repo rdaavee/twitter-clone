493d1eb655053261f98ae987c8ede34639ebf042
rdaavee <rdavee0412@gmail.com>
2024-04-30 21:37:50 +0800

    added suggested users functionality (working in postman)

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -3,11 +3,13 @@ import { protectRoute } from "../middleware/protectRoute.js";
 import {
   getUserProfile,
   followUnfollowUser,
+  getSuggestedUser,
 } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/profile/:username", protectRoute, getUserProfile);
+router.get("/suggested", protectRoute, getSuggestedUser);
 
 router.post("/follow/:id", protectRoute, followUnfollowUser);
 
