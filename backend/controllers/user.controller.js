940156ce5aabf4a423a22def9399a6fa347b7364
rdaavee <rdavee0412@gmail.com>
2024-05-02 11:49:49 +0800

    added update functionality for users' information

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 import Notification from "../models/notification.model.js";
 
 export const getUserProfile = async (req, res) => {
@@ -86,6 +87,55 @@ export const getSuggestedUser = async (req, res) => {
     res.status(200).json(suggestedUsers);
   } catch (error) {
     console.log("Error in getSuggestedUser: ", error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const { fullName, email, username, currentPassword, newPassword } = req.body;
+
+  const userId = req.user._id;
+
+  try {
+    let user = await User.findById(userId);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    if (
+      (!newPassword && currentPassword) ||
+      (!currentPassword && newPassword)
+    ) {
+      return res.status(400).json({
+        error: "Please provide both current password and new password",
+      });
+    }
+
+    if (currentPassword && newPassword) {
+      const isMatch = await bcrypt.compare(currentPassword, user.password);
+
+      if (!isMatch)
+        return res.status(400).json({ error: "Current password is incorrect" });
+
+      if (newPassword.length < 6) {
+        return res
+          .status(400)
+          .json({ error: "Password must be at least 8 characters long!" });
+      }
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(newPassword, salt);
+    }
+
+    user.fullName = fullName || user.fullName;
+    user.email = email || user.email;
+    user.username = username || user.username;
+
+    user = await user.save();
+    
+    user.password = null;
+
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log("Error in updateUser: ", error.message);
     res.status(500).json({ message: error.message });
   }
 };
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,6 +4,7 @@ import {
   getUserProfile,
   followUnfollowUser,
   getSuggestedUser,
+  updateUser,
 } from "../controllers/user.controller.js";
 
 const router = express.Router();
@@ -12,5 +13,6 @@ router.get("/profile/:username", protectRoute, getUserProfile);
 router.get("/suggested", protectRoute, getSuggestedUser);
 
 router.post("/follow/:id", protectRoute, followUnfollowUser);
+router.post("/update", protectRoute, updateUser);
 
 export default router;
