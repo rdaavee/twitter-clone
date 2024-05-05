eb947248660d55f2f54c7f370686365d2141603e
rdaavee <rdavee0412@gmail.com>
2024-05-05 22:54:34 +0800

    added comment functionality (working in mongodb and postman)

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { createPost, deletePost } from "../controllers/post.controller.js";
+import {
+  createPost,
+  deletePost,
+  commentOnPost,
+} from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", protectRoute, createPost);
+router.post("/comment/:id", protectRoute, commentOnPost);
 
 router.delete("/:id", protectRoute, deletePost);
 
