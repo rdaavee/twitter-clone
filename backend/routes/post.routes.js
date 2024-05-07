d3264ee4311ebb99f5de85f70e55e174a40dc399
rdaavee <rdavee0412@gmail.com>
2024-05-07 16:21:36 +0800

    added like/unlike, getAllPosts, getLikedPosts, and getFollowingPosts functionality

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -4,13 +4,20 @@ import {
   createPost,
   deletePost,
   commentOnPost,
+  likeUnlikePost,
+  getAllPosts,
+  getLikedPosts,
+  getFollowingPosts,
 } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
+router.get("/all", protectRoute, getAllPosts);
+router.get("/following", protectRoute, getFollowingPosts); //following only
+router.get("/likes/:id", protectRoute, getLikedPosts);
 router.post("/create", protectRoute, createPost);
 router.post("/comment/:id", protectRoute, commentOnPost);
-
+router.post("/like/:id", protectRoute, likeUnlikePost);
 router.delete("/:id", protectRoute, deletePost);
 
 export default router;
