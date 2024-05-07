23e0ac3838f2eb638ada2d404f3b714a77bc5332
rdaavee <rdavee0412@gmail.com>
2024-05-07 21:21:01 +0800

    added getUserPosts functionality

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -8,6 +8,7 @@ import {
   getAllPosts,
   getLikedPosts,
   getFollowingPosts,
+  getUserPosts,
 } from "../controllers/post.controller.js";
 
 const router = express.Router();
@@ -15,6 +16,7 @@ const router = express.Router();
 router.get("/all", protectRoute, getAllPosts);
 router.get("/following", protectRoute, getFollowingPosts); //following only
 router.get("/likes/:id", protectRoute, getLikedPosts);
+router.get("/user/:username", protectRoute, getUserPosts);
 router.post("/create", protectRoute, createPost);
 router.post("/comment/:id", protectRoute, commentOnPost);
 router.post("/like/:id", protectRoute, likeUnlikePost);
