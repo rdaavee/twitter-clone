65d44005979d92ebd8ffc1d54e7133d900fec132
rdaavee <rdavee0412@gmail.com>
2024-05-09 00:47:36 +0800

    created login and signup pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+
+import LoginPage from "./pages/auth/login/LoginPage"
+import SignUpPage from "./pages/auth/signup/SignUpPage"
 
 function App() {
 
     return (
-        <>
-            <p>Frontend Setup</p>
-        </>
+        <div className='flex max-w-6xl mx-auto'>
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+            </Routes>
+        </div>
     )
 }
 
