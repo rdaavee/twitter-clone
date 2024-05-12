44900ec22183a7834c9cda6c4d5e443ad3f01ded
rdaavee <rdavee0412@gmail.com>
2024-05-12 23:03:04 +0800

    created a sidebar and right panel with dummy users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,21 @@ import { Route, Routes } from "react-router-dom"
 
 import LoginPage from "./pages/auth/login/LoginPage"
 import SignUpPage from "./pages/auth/signup/SignUpPage"
+import HomePage from "./pages/home/HomePage"
+import Sidebar from "./components/common/Sidebar"
+import RightPanel from "./components/common/RightPanel"
 
 function App() {
 
     return (
         <div className='flex max-w-6xl mx-auto'>
+            <Sidebar />
             <Routes>
+                <Route path="/" element={<HomePage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/signup" element={<SignUpPage />} />
             </Routes>
+            <RightPanel />
         </div>
     )
 }
