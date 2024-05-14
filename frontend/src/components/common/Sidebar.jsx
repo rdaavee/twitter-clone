3059bc473c9465a6d522fe4fe2530b9e2f4f586e
rdaavee <rdavee0412@gmail.com>
2024-05-14 22:54:00 +0800

    implemented useMutation and toast

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -6,12 +6,38 @@ import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
 
+import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
 const Sidebar = () => {
     const data = {
         fullName: "Ranier Tan",
         username: "ranier",
         profileImg: "/avatars/userImage1.jpg",
     };
+
+    const { mutate: logout } = useMutation({
+        mutationFn: async () => {
+            try {
+                const res = await fetch("/api/auth/logout", {
+                    method: "POST",
+                });
+                const data = await res.json;
+
+                if (!res.ok) {
+                    throw new Error(data.error || "Something went wrong!");
+                }
+            } catch (error) {
+                throw new Error(error);
+            }
+        },
+        onSuccess: () => {
+            toast.success("Logout successfully")
+        },
+        onError: () => {
+            toast.error("Logout failed");
+        }
+    })
 
     return (
         <div className='md:flex-[2_2_0] w-18 max-w-52'>
@@ -64,7 +90,10 @@ const Sidebar = () => {
                                 <p className='text-white font-bold text-sm w-20 truncate'>{data?.fullName}</p>
                                 <p className='text-slate-500 text-sm'>@{data?.username}</p>
                             </div>
-                            <BiLogOut className='w-5 h-5 cursor-pointer' />
+                            <BiLogOut className='w-5 h-5 cursor-pointer' onClick={(e) => {
+                                e.preventDefault();
+                                logout()
+                            }} />
                         </div>
                     </Link>
                 )}
